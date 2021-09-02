23482f78d932a98f25f0ebe418219723d0abfb7c
Rajat Singh <rsingh1734@gmail.com>
2021-09-02 17:06:10 +0530

    Test Commit1

---
+++ cloud-notebook-backend/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.export = mongoose.model('user', UserSchema);
