26f7863e2f8d5fd983d3cc46d994307e7298277a
Rajat Singh <rsingh1734@gmail.com>
2023-12-25 17:49:08 +0530

    revamped on main

---
+++ cloud-notebook-frontend/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import NoteState from './Context/notes/NotesState';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Alert from './components/Alert';
+
+function App() {
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({ msg: message, type: type });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
+
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          {alert && <Alert type={alert.type} msg={alert.msg} />}
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home showAlert={showAlert} />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login showAlert={showAlert} />} />
+              <Route path="/signup" element={<Signup showAlert={showAlert} />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
+}
+
+export default App;
